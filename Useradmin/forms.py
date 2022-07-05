e1d299431a514ebb675705531ba3a84672d1df2d
Ilokorb <s50402@bht-berlin.de>
2022-07-05 23:38:22 +0200

    regestration for AbstractUser

---
+++ Useradmin/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import MyUser
+
+
+class MySignUpForm(UserCreationForm):
+    class Meta:
+        model = MyUser
+        fields = ('username',)  # 'first_name', 'last_name', 'email', 'profile_picture')
+    # password ist wegen UserCreationForm schon mit dabei
--- Useradmin/views.py
+++ Useradmin/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import (
+    login as auth_login,
+)
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic.base import TemplateView
+from .forms import MySignUpForm
+from .models import MyUser
 
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+class SignUp(generic.CreateView):  # jetzt zu mein SignupViw verandern
+    form_class = MySignUpForm  # eigen Form erstellen und dann darauf anpasse, imports bedenken
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
