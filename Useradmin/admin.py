00f9a07ba7cf7419a45384a753e45d304d7e9e3a
Ilokorb <s50402@bht-berlin.de>
2022-07-06 19:37:15 +0200

    added Products and Users to adminsite

--- Useradmin/admin.py
+++ Useradmin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MyUser
+from Product.models import Product
 
 # Register your models here.
+
+admin.site.register(MyUser)
+admin.site.register(Product)
