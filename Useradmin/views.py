98407c9cb1f202bb99a65a5039f405bdfd18092a
Ilokorb <s50402@bht-berlin.de>
2022-07-06 13:44:47 +0200

    login for AbstractUser

--- Useradmin/views.py
+++ Useradmin/views.py
@@ -10,7 +10,17 @@ from .forms import MySignUpForm
 from .models import MyUser
 
 
-class SignUp(generic.CreateView):  # jetzt zu mein SignupViw verandern
+class MySignUpView(generic.CreateView):  # jetzt zu mein SignupViw verandern
     form_class = MySignUpForm  # eigen Form erstellen und dann darauf anpasse, imports bedenken
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class MyLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in. PERFORM CUSTOM CODE."""
+        auth_login(self.request, form.get_user())
+        # form.get_user().execute_after_login()  # Custom code
+        return HttpResponseRedirect(self.get_success_url())
