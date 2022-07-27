c04e5a22ab9f296d6860b5c685f761155c7d8535
Drakronem <canadischesieben@web.de>
2022-07-27 20:50:59 +0200

    aded links to the websites to navigate + implemented redirected from root to home

--- ScratchTreeAdventures/urls.py
+++ ScratchTreeAdventures/urls.py
@@ -18,8 +18,10 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+                  path('', lambda req: redirect('/home/')),
                   path('admin/', admin.site.urls),
                   path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
                   path('useradmin/', include('Useradmin.urls')),
