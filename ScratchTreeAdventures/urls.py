f0f0a598b70999653583bc8199533ae1eb99aea9
JustMaxOkay <93137285+JustMaxOkay@users.noreply.github.com>
2022-07-28 16:01:27 +0200

    Customer Service Portal, User-Profile, User-List

--- ScratchTreeAdventures/urls.py
+++ ScratchTreeAdventures/urls.py
@@ -14,20 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
-                  path('useradmin/', include('Useradmin.urls')),
-                  path('useradmin/', include('django.contrib.auth.urls')),
-                  path('products/', include('Product.urls')),
-                  path('shoppingcart/', include('Shoppingcart.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('', lambda req: redirect('/home/')),
+    path('admin/', admin.site.urls),
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('useradmin/', include('Useradmin.urls')),
+    path('useradmin/', include('django.contrib.auth.urls')),
+    path('products/', include('Product.urls')),
+    path('shoppingcart/', include('Shoppingcart.urls')),
+    path('customerservice/', include('Customerservice.urls')),
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
