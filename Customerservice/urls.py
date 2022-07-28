cee16c4a40359a24141cf1ed9ed85b87e5cd1b30
JustMaxOkay <93137285+JustMaxOkay@users.noreply.github.com>
2022-07-28 17:17:17 +0200

    Pain

---
+++ Customerservice/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    path('delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('edit/<int:pk>/', views.CommentEditView.as_view(), name='comment-edit'),
+    path('editdelete/<int:pk>/', views.comment_edit_delete, name='comment-edit-delete'),
+    path('portal/', TemplateView.as_view(template_name='portal.html'), name='portal'),
+]
