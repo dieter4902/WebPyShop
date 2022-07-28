3baf1a78c67c151f0f9abc51219c8634a4a44108
adrian_ <adrianjost1@gmx.de>
2022-07-28 02:29:44 +0200

    fixed star search

--- Product/forms.py
+++ Product/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Vote, Comment
+from .models import Product, Comment
 
 
 class ProductForm(forms.ModelForm):
@@ -27,24 +27,24 @@ class SearchForm(forms.ModelForm):
     width = forms.IntegerField(required=False, max_value=500, min_value=0)
     length = forms.IntegerField(required=False, max_value=500, min_value=0)
     weight = forms.IntegerField(required=False, max_value=500, min_value=0)
-    stockwerke = forms.IntegerField(required=False, max_value=20,min_value=0)
+    stockwerke = forms.IntegerField(required=False, max_value=20, min_value=0)
     hoehlen = forms.IntegerField(required=False, max_value=20, min_value=0)
-    price = forms.IntegerField(required=False,  min_value=0)
+    price = forms.IntegerField(required=False, min_value=0)
     MATERIALS = [
         ("", ""),
         ("p", "Plüsch"),
         ("s", "Samt")
     ]
     material = forms.ChoiceField(initial='', choices=MATERIALS, required=False, )
+    stars = forms.IntegerField(required=False, max_value=5, min_value=0)
 
     class Meta:
         model = Product
-        fields = ['name', 'description', 'brand', 'material', 'height']
+        fields = ['name', 'description', 'brand', 'material', 'height', 'stars']
 
-
-class SearchStarsForm(forms.ModelForm):
-    stars = forms.IntegerField(required=False, max_value=5, min_value=1)
-
-    class Meta:
-        model = Vote
-        fields = ['stars']
+# class SearchStarsForm(forms.ModelForm):
+#    stars = forms.IntegerField(required=False, max_value=5, min_value=1)
+#
+#    class Meta:
+#        model = Product
+#        fields = ['stars']
