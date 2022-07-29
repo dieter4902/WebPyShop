62608d288556df2d2e1d9e2d7da14715d0e8df23
JustMaxOkay <93137285+JustMaxOkay@users.noreply.github.com>
2022-07-29 08:37:01 +0200

    Shoppin Cart hat nicht funktioniert, jetzt schon aber es gibt nen Fehler wenn man versucht nen leeren Shopping Cart zu leeren

--- Shoppingcart/views.py
+++ Shoppingcart/views.py
@@ -9,30 +9,12 @@ def show_shopping_cart(request):
 
     if request.method == 'POST':
         if 'empty' in request.POST:
-            shopping_cart_is_empty = True
-            shopping_cart_items = None
-            total = Decimal(0.0)  # Default without Decimal() would be type float!
+            ShoppingCart.objects.get(myuser=request.user).delete()
 
-            myuser = request.user
-            if myuser.is_authenticated:
-                shopping_carts = ShoppingCart.objects.filter(myuser=myuser)
-                if shopping_carts:
-                    shopping_cart = shopping_carts.first()
-                    shopping_cart_is_empty = False
-                    shopping_cart_items = ShoppingCartItem.objects.filter(shopping_cart=shopping_cart)
-                    total = shopping_cart.get_total()
-            if shopping_cart_is_empty:
-                context = {'shopping_cart_is_empty': shopping_cart_is_empty,
-                        'shopping_cart_items': shopping_cart_items,
-                        'total': total}
-                return render(request, 'shopping-cart.html', context)
-            else:
-                ShoppingCart.objects.get(myuser=request.user).delete()
-
-                context = {'shopping_cart_is_empty': True,
-                        'shopping_cart_items': None,
-                        'amount': 0.0}
-                return render(request, 'shopping-cart.html', context)
+            context = {'shopping_cart_is_empty': True,
+                       'shopping_cart_items': None,
+                       'amount': 0.0}
+            return render(request, 'shopping-cart.html', context)
 
         elif 'pay' in request.POST:
             return redirect('shopping-cart-pay')
